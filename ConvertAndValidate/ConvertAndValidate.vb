d91efc5f100cb96fc43a7740168d61b9c195fe87
Sebastian Soto <thefirstcoolone@gmail.com>
2021-09-21 11:51:07 -0600

    dave

--- ConvertAndValidate/ConvertAndValidate.vb
+++ ConvertAndValidate/ConvertAndValidate.vb
@@ -6,9 +6,17 @@
 Option Explicit On
 Option Strict On
 Module ConvertAndValidate
-
     Sub Main()
+        Dim userInput As String
+        Dim numberReturn As Integer
 
     End Sub
+
+    Function ReturnNumber(ByVal userInput ByRef numberReturn) As Integer
+
+
+
+
+    End Function
 
 End Module
